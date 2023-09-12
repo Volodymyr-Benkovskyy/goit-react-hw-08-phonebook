07ba202d02c51d2a6bed76d468f4002744b3d4b8
Volodymyr-Benkovskyy <123126442+Volodymyr-Benkovskyy@users.noreply.github.com>
2023-09-12 16:17:08 +0000

    Deploying to gh-pages from @ Volodymyr-Benkovskyy/goit-react-hw-08-phonebook@504e5fc1a9bef3a927fd5a63995bf384174d92c8 🚀

---
+++ static/js/76.196726bd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[76],{76:function(e,t,r){r.r(t);var n=r(329);t.default=function(){return(0,n.jsx)("h1",{style:{textAlign:"center"},children:"Welcome to our amazing site!!!"})}}}]);
+//# sourceMappingURL=76.196726bd.chunk.js.map
